f330080875f93318f3f8a12c0ad27f2d14942258
SaherAAKadri <32628091+SaherAAKadri@users.noreply.github.com>
2021-03-01 13:11:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
+	//"github.com/gorilla/mux"
+	"github.com/gorilla/mux/mux"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -16,8 +19,12 @@ type Database struct {
 	//path
 }
 
-type Urls struct {
-	LongUrl  string
+type LUrls struct {
+	LongUrl string
+	//ShortUrl string
+}
+
+type SUrls struct {
 	ShortUrl string
 }
 
@@ -34,24 +41,40 @@ func OpenDatabase() (*gorm.DB, error) {
 		return nil, err
 	}
 	//defer db.Close()
-	db.Migrator().CreateTable(&Urls{})
+	db.Migrator().CreateTable(&LUrls{})
+
 	return db, nil
 }
 
-func main() {
-	OpenDatabase()
-	fmt.Println("Tiny URL API consumption")
+func WelcomePage(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Welcome to Url Shortner --> Created by Saher AA Kadri!\n")
+}
 
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
-		os.Exit(1)
+func CreateShortUrl(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	lurl := LUrls{}
+	err := decoder.Decode(&lurl)
+
+	if err != nil {
+		panic(err)
 	}
 
+	fmt.Println(lurl.LongUrl)
+	OpenDatabase()
+	fmt.Println("Tiny URL API consumption")
+	/*
+		if len(os.Args) != 2 {
+			fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
+			os.Exit(1)
+		}
+	*/
 	baseUrl := "http://tinyurl.com/api-create.php?url="
-	urlToShorten := os.Args[1]
+	//urlToShorten := os.Args[1]
+	urlToShorten := lurl.LongUrl
 	getReqUrl := baseUrl + urlToShorten
 
 	response, err := http.Get(getReqUrl)
+	//fmt.Println(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	} else {
@@ -61,5 +84,11 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}
 
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/welcome-url-shortner", WelcomePage).Methods("GET")
+	r.HandleFunc("/create-short-url", CreateShortUrl).Methods("POST")
+	http.ListenAndServe(":8001", r)
 }
